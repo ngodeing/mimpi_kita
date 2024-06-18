4ccd3ca1a03ce2f88720ba42c7b3fbc9a176ee52
Taufik Nurrohman <onlytaufik47@gmail.com>
2024-06-18 18:50:34 +0700

    memperbaiki pencarian

--- lib/home_page/home_page_widget.dart
+++ lib/home_page/home_page_widget.dart
@@ -106,7 +106,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                                 padding: EdgeInsetsDirectional.fromSTEB(
                                     15.0, 0.0, 0.0, 0.0),
                                 child: Text(
-                                  'Cari Donasi...',
+                                  'Cari Penggalangan...',
                                   style: FlutterFlowTheme.of(context)
                                       .bodyMedium
                                       .override(

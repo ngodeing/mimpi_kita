2ce2d914b500861f443dc86b881c0ca529d1be43
lintangkilau <112309985+lintangkilau@users.noreply.github.com>
2024-06-18 18:22:15 +0700

    edit

--- lib/detail_beasiswa/detail_beasiswa_widget.dart
+++ lib/detail_beasiswa/detail_beasiswa_widget.dart
@@ -85,7 +85,7 @@ class _DetailBeasiswaWidgetState extends State<DetailBeasiswaWidget> {
                           style:
                               FlutterFlowTheme.of(context).bodyMedium.override(
                                     fontFamily: 'Readex Pro',
-                                    fontSize: 40.0,
+                                    fontSize: 33.0,
                                     letterSpacing: 0.0,
                                     fontWeight: FontWeight.w600,
                                   ),
@@ -98,7 +98,7 @@ class _DetailBeasiswaWidgetState extends State<DetailBeasiswaWidget> {
                         padding:
                             EdgeInsetsDirectional.fromSTEB(0.0, 5.0, 0.0, 0.0),
                         child: Text(
-                          'Tenggat Minggu, 16 Juni 2024',
+                          'Tenggat Minggu, 22 Juni 2024',
                           style:
                               FlutterFlowTheme.of(context).bodyMedium.override(
                                     fontFamily: 'Readex Pro',
@@ -110,9 +110,9 @@ class _DetailBeasiswaWidgetState extends State<DetailBeasiswaWidget> {
                     ),
                     Padding(
                       padding:
-                          EdgeInsetsDirectional.fromSTEB(0.0, 20.0, 0.0, 0.0),
+                          EdgeInsetsDirectional.fromSTEB(0.0, 30.0, 0.0, 0.0),
                       child: ClipRRect(
-                        borderRadius: BorderRadius.circular(8.0),
+                        borderRadius: BorderRadius.circular(9.0),
                         child: Image.asset(
                           'assets/images/1522298726_Tentang_Kami_-_Tentang_DBP_preview.jpg',
                           width: double.infinity,
@@ -125,9 +125,9 @@ class _DetailBeasiswaWidgetState extends State<DetailBeasiswaWidget> {
                       alignment: AlignmentDirectional(-1.0, 0.0),
                       child: Padding(
                         padding:
-                            EdgeInsetsDirectional.fromSTEB(0.0, 20.0, 0.0, 0.0),
+                            EdgeInsetsDirectional.fromSTEB(0.0, 30.0, 0.0, 0.0),
                         child: Text(
-                          'Sejak 1984, Djarum Foundation terus konsisten dalam memberikan kontribusi terhadap dunia pendidikan di Indonesia. Langkah ini diawali kesadaran bahwa pendidikan merupakan salah satu upaya untuk meningkatkan kesejahteraan masyarakat dan bangsa dalam mewujudkan masa depan yang lebih baik.\n\nDjarum Foundation turut berperan aktif dalam memajukan pendidikan di Indonesia melalui program beasiswa prestasi (Merit based Scholarship) yang dikenal sebagai Djarum Beasiswa Plus bagi mahasiswa berprestasi di Indonesia.Perbedaan Djarum Beasiswa Plus dengan program beasiswa lain adalah selain mendapatkan dana beasiswa selama satu tahun, Beswan Djarum (sebutan bagi penerima program Djarum Beasiswa Plus) juga mendapatkan berbagai macam pelatihan ketrampilan lunak atau soft skills guna mempersiapkan mereka menjadi calon pemimpin masa depan bangsa. \n\nPelatihan ini meliputi Nation Building, Character Building, Leadership Development, Competition Challenges, serta International Exposure. Tidak hanya berhenti sampai di sini, melalui program Community Empowerment, Beswan Djarum juga diberikan kesempatan untuk menerapkan berbagai ketrampilan lunak yang telah diperoleh dengan melibatkan diri secara langsung dalam memberikan jalan keluar pada suatu permasalahan sosial di lingkungan tempat mereka berada.Melalui pembudayaan dan pemberdayaan para penerima program Djarum Beasiswa Plus ini, rangkaian pelatihan tersebut dimaksudkan guna menyerasikan antara pencapaian akademik (hard skills) yang diperoleh di perguruan tinggi dengan berbagai ketrampilan lunak (soft skills) yang diperoleh dari program ini. Tujuannya agar Beswan Djarum di kemudian hari menjadi pemimpin masa depan bangsa yang cakap secara intelegensia maupun emosional.Lebih dari 13.397 mahasiswa berprestasi telah menjadi penerima program. Mereka berasal dari berbagai macam latar belakang pendidikan di lebih dari 127 perguruan tinggi unggulan di seluruh 34 provinsi di Indonesia. Sebuah bukti keseriusan Djarum Foundation dalam komitmennya membangun bangsa melalui pendidikan.',
+                          'Sejak 1984, Djarum Foundation terus konsisten dalam memberikan kontribusi terhadap dunia pendidikan di Indonesia. Langkah ini diawali kesadaran bahwa pendidikan merupakan salah satu upaya untuk meningkatkan kesejahteraan masyarakat dan bangsa dalam mewujudkan masa depan yang lebih baik.\n\nDjarum Foundation turut berperan aktif dalam memajukan pendidikan di Indonesia melalui program beasiswa prestasi (Merit based Scholarship) yang dikenal sebagai Djarum Beasiswa Plus bagi mahasiswa berprestasi tinggi di Indonesia.Perbedaan Djarum Beasiswa Plus dengan program beasiswa lain adalah selain mendapatkan dana beasiswa selama satu tahun, Beswan Djarum (sebutan bagi penerima program Djarum Beasiswa Plus) juga mendapatkan berbagai macam pelatihan ketrampilan lunak atau soft skills guna mempersiapkan mereka menjadi calon pemimpin masa depan bangsa. \n\nPelatihan ini meliputi Nation Building, Character Building, Leadership Development, Competition Challenges, serta International Exposure. Tidak hanya berhenti sampai di sini, melalui program Community Empowerment, Beswan Djarum juga diberikan kesempatan untuk menerapkan berbagai ketrampilan lunak yang telah diperoleh dengan melibatkan diri secara langsung dalam memberikan jalan keluar pada suatu permasalahan sosial di lingkungan tempat mereka berada.Melalui pembudayaan dan pemberdayaan para penerima program Djarum Beasiswa Plus ini, rangkaian pelatihan tersebut dimaksudkan guna menyerasikan antara pencapaian akademik (hard skills) yang diperoleh di perguruan tinggi dengan berbagai ketrampilan lunak (soft skills) yang diperoleh dari program ini. Tujuannya agar Beswan Djarum di kemudian hari menjadi pemimpin masa depan bangsa yang cakap secara intelegensia maupun emosional.Lebih dari 13.397 mahasiswa berprestasi telah menjadi penerima program. Mereka berasal dari berbagai macam latar belakang pendidikan di lebih dari 127 perguruan tinggi unggulan di seluruh 34 provinsi di Indonesia. Sebuah bukti keseriusan Djarum Foundation dalam komitmennya membangun bangsa melalui pendidikan.',
                           style:
                               FlutterFlowTheme.of(context).bodyMedium.override(
                                     fontFamily: 'Readex Pro',
@@ -138,7 +138,7 @@ class _DetailBeasiswaWidgetState extends State<DetailBeasiswaWidget> {
                     ),
                     Padding(
                       padding:
-                          EdgeInsetsDirectional.fromSTEB(0.0, 50.0, 0.0, 20.0),
+                          EdgeInsetsDirectional.fromSTEB(0.0, 45.0, 0.0, 20.0),
                       child: FFButtonWidget(
                         onPressed: () async {
                           await launchURL('https://djarumbeasiswaplus.org/');
@@ -148,7 +148,7 @@ class _DetailBeasiswaWidgetState extends State<DetailBeasiswaWidget> {
                           width: double.infinity,
                           height: 40.0,
                           padding: EdgeInsetsDirectional.fromSTEB(
-                              20.0, 0.0, 20.0, 0.0),
+                              25.0, 0.0, 25.0, 0.0),
                           iconPadding: EdgeInsetsDirectional.fromSTEB(
                               0.0, 0.0, 0.0, 0.0),
                           color: Color(0xFFFEC739),

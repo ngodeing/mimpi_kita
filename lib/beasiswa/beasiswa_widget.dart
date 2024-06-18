aca77716f247b09d01168f785079b72514d96e8a
coro-kunnnnnn <finkazhr@gmail.com>
2024-06-18 22:31:49 +0700

    mengganti ukuran border radius

--- lib/beasiswa/beasiswa_widget.dart
+++ lib/beasiswa/beasiswa_widget.dart
@@ -107,8 +107,8 @@ class _BeasiswaWidgetState extends State<BeasiswaWidget> {
                             borderRadius: BorderRadius.only(
                               bottomLeft: Radius.circular(0.0),
                               bottomRight: Radius.circular(0.0),
-                              topLeft: Radius.circular(30.0),
-                              topRight: Radius.circular(30.0),
+                              topLeft: Radius.circular(35.0),
+                              topRight: Radius.circular(35.0),
                             ),
                           ),
                           child: Padding(
